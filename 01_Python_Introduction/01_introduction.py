ea025961d940f3fc418acdd0d6b091c4dbaf7afd
subham-2029 <subham.mallick2025@lpu.in>
2025-12-25 15:08:17 +0530

    Create 01_introduction.py

---
+++ 01_Python_Introduction/01_introduction.py
@@ -0,0 +1,8 @@
+#Writing Your First Program
+
+print("Hello, World!")
+
+# Terminal Run Command:
+# python 01_introduction.py
+
+# Upon Running in IDE, using f5 key or fn+f5 key, this prints "Hello, World!" to the console.
